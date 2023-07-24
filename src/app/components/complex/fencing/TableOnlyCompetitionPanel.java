254808616a36f8938f8284855ab54bb8ee20871a
SzDave <szantodavid.mail@gmail.com>
2023-07-25 00:42:59 +0200

    Finish the TableOnlyCompetitionPanel

--- src/app/components/complex/fencing/TableOnlyCompetitionPanel.java
+++ src/app/components/complex/fencing/TableOnlyCompetitionPanel.java
@@ -1,6 +1,7 @@
 package app.components.complex.fencing;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.util.List;
 
@@ -10,10 +11,12 @@ import app.components.complex.other.NumberedFencerLabel;
 import app.components.textcontainers.XTextField;
 import app.frame.XFrame;
 import app.other.Fencer;
+import support.appdata.AppearanceData;
 import support.appdata.SizeData;
 import support.constants.PositionConstants;
 import support.framework.appearances.BasicAppearance;
 import support.framework.appearances.ColoredAppearance;
+import support.framework.builders.CustomAppearanceBuilder;
 import support.framework.interfaces.Appearance;
 
 public final class TableOnlyCompetitionPanel extends AbstractCompetitionPanel {
@@ -49,8 +52,6 @@ public final class TableOnlyCompetitionPanel extends AbstractCompetitionPanel {
 
                 this.scrollPanel.addComponent(fencerLabel);
             }
-
-
             for (int y = 0; y < tableSize; y += 2) {
                 final XTextField fencerInputField = new XTextField(
                     SizeData.GAP + SizeData.NARROW_BUTTON_WIDTH + (SizeData.WIDE_BUTTON_WIDTH),
@@ -102,47 +103,18 @@ public final class TableOnlyCompetitionPanel extends AbstractCompetitionPanel {
                         SizeData.WIDE_BUTTON_WIDTH,
                         SizeData.BUTTON_HEIGHT,
                         this.frame,
-                        BasicAppearance.WHITE_BORDERED
+                        BasicAppearance.BLACK_BORDERED
                     );
 
                     this.scrollPanel.addComponent(fencerInputField);
                 }
             }
 
-            if (tableSize >= 64)
-            for (int y = 0; y < tableSize; y += 32) {
-                final XTextField fencerInputField = new XTextField(
-                    SizeData.GAP + SizeData.NARROW_BUTTON_WIDTH + (SizeData.WIDE_BUTTON_WIDTH * 5),
-                    (SizeData.BUTTON_HEIGHT * (y + 16)) + (SizeData.BUTTON_HEIGHT * (y + 15)),
-                    SizeData.WIDE_BUTTON_WIDTH,
-                    SizeData.BUTTON_HEIGHT,
-                    this.frame,
-                    ColoredAppearance.RED_BORDERED
-                );
-
-                this.scrollPanel.addComponent(fencerInputField);
-            }
-        }
-        // Generate the first column with fencerListSize
-        else {
-            for (int i = 0; i < tableSize; i++) {
-                final NumberedFencerLabel fencerLabel = new NumberedFencerLabel(SizeData.GAP, ((SizeData.BUTTON_HEIGHT) * (i) + (SizeData.BUTTON_HEIGHT * i)),
-                    String.valueOf(i + 1), "", this.frame, BasicAppearance.BLACK);
-
-                this.scrollPanel.addComponent(fencerLabel);
-            }
-
-            int yDif = 0;
-            int yOffset = 0;
-
-            for (int x = 1; x < nPowerOf2; x++) {
-                yDif += 2;
-                yOffset += 1;
-
-                for (int y = 0; y < tableSize; y += yDif) {
+            if (tableSize >= 64) {
+                for (int y = 0; y < tableSize; y += 32) {
                     final XTextField fencerInputField = new XTextField(
-                        SizeData.GAP + SizeData.NARROW_BUTTON_WIDTH + (SizeData.WIDE_BUTTON_WIDTH * x),
-                        (SizeData.BUTTON_HEIGHT * (y + yOffset)) + (SizeData.BUTTON_HEIGHT * (y + (yOffset - 1))),
+                        SizeData.GAP + SizeData.NARROW_BUTTON_WIDTH + (SizeData.WIDE_BUTTON_WIDTH * 5),
+                        (SizeData.BUTTON_HEIGHT * (y + 16)) + (SizeData.BUTTON_HEIGHT * (y + 15)),
                         SizeData.WIDE_BUTTON_WIDTH,
                         SizeData.BUTTON_HEIGHT,
                         this.frame,
@@ -152,6 +124,84 @@ public final class TableOnlyCompetitionPanel extends AbstractCompetitionPanel {
                     this.scrollPanel.addComponent(fencerInputField);
                 }
             }
+
+            if (tableSize >= 128) {
+                for (int y = 0; y < tableSize; y += 64) {
+                    final XTextField fencerInputField = new XTextField(
+                        SizeData.GAP + SizeData.NARROW_BUTTON_WIDTH + (SizeData.WIDE_BUTTON_WIDTH * 6),
+                        (SizeData.BUTTON_HEIGHT * (y + 32)) + (SizeData.BUTTON_HEIGHT * (y + 31)),
+                        SizeData.WIDE_BUTTON_WIDTH,
+                        SizeData.BUTTON_HEIGHT,
+                        this.frame,
+                        ColoredAppearance.RED_BORDERED
+                    );
+
+                    this.scrollPanel.addComponent(fencerInputField);
+                }
+            }
+
+            // Create the gold medal bout's input
+            final XTextField fencerInputField = new XTextField(
+                SizeData.GAP + SizeData.NARROW_BUTTON_WIDTH + (SizeData.WIDE_BUTTON_WIDTH * nPowerOf2),
+                SizeData.BUTTON_HEIGHT * (tableSize - 1),
+                SizeData.WIDE_BUTTON_WIDTH,
+                SizeData.BUTTON_HEIGHT,
+                this.frame,
+                new CustomAppearanceBuilder()
+                    .addMainBackground(Color.black)
+                    .addMainForeground(Color.red)
+                    .addBorder(AppearanceData.RED_BORDER)
+                    .addFont(AppearanceData.MAIN_FONT_B)
+                    .build()
+            );
+
+            this.scrollPanel.addComponent(fencerInputField);
+        }
+        // Generate the whole table with a size of 8
+        else {
+            for (int i = 0; i < tableSize; i++) {
+                final NumberedFencerLabel fencerLabel = new NumberedFencerLabel(SizeData.GAP, ((SizeData.BUTTON_HEIGHT * i) + (SizeData.BUTTON_HEIGHT * i)),
+                    String.valueOf(i + 1), "", this.frame, BasicAppearance.BLACK);
+
+                this.scrollPanel.addComponent(fencerLabel);
+            }
+
+            int yOffset = 1;
+            int tempTableSize = tableSize / 2;
+
+            for (int x = 1; x < nPowerOf2; x++) {
+                for (int y = 0; y < tempTableSize; y++) {
+                    final XTextField fencerInputField = new XTextField(
+                        SizeData.GAP + SizeData.NARROW_BUTTON_WIDTH + (SizeData.WIDE_BUTTON_WIDTH * x),
+                        (SizeData.BUTTON_HEIGHT * yOffset),
+                        SizeData.WIDE_BUTTON_WIDTH,
+                        SizeData.BUTTON_HEIGHT,
+                        this.frame,
+                        BasicAppearance.BLACK_BORDERED
+                    );
+                    yOffset += 4 * x;
+                    this.scrollPanel.addComponent(fencerInputField);
+                }
+                tempTableSize /= 2;
+                yOffset = 3;
+            }
+
+            // Create the gold medal bout's input
+            final XTextField fencerInputField = new XTextField(
+                SizeData.GAP + SizeData.NARROW_BUTTON_WIDTH + (SizeData.WIDE_BUTTON_WIDTH * nPowerOf2),
+                SizeData.BUTTON_HEIGHT * (tableSize - 1),
+                SizeData.WIDE_BUTTON_WIDTH,
+                SizeData.BUTTON_HEIGHT,
+                this.frame,
+                new CustomAppearanceBuilder()
+                    .addMainBackground(Color.black)
+                    .addMainForeground(Color.red)
+                    .addBorder(AppearanceData.RED_BORDER)
+                    .addFont(AppearanceData.MAIN_FONT_B)
+                    .build()
+            );
+
+            this.scrollPanel.addComponent(fencerInputField);
         }
     }
 
